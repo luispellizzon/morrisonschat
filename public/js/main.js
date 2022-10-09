ade87160c4e909268ef5e130006cc2e571b7b7e0
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-09 22:39:37 +0100

    catching the message submit and display on the chat

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
+const chatForm = document.getElementById("chat-form");
+
 const socket = io();
 
 socket.on("message", (message) => {
   console.log(message);
+});
+
+// Message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.msg.value;
 });
