16c6551ea049834d13ad8a0f62306201e4dfec85
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-09 21:40:26 +0100

    Setting our static folder on server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
+
+//Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = 3000 || process.env.PORT;
 
