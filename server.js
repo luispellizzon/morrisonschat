376c70f8eb0a2afed10b5a431087893f5bb7b758
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-09 21:44:48 +0100

    Socket io setted

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const path = require("path");
-
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// Run when client connects
+io.on("connection", (socket) => {
+  console.log("New sk cnt");
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server on: ${PORT}`));
+server.listen(PORT, () => console.log(`Server on: ${PORT}`));
